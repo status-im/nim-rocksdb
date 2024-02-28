be4b3ad3245ba3ddafe7a02a1b719792a8296b25
web3-developer <51288821+web3-developer@users.noreply.github.com>
2024-02-28 12:49:20 +0800

    Completed transactiondb tests.

--- tests/test_all.nim
+++ tests/test_all.nim
@@ -1,5 +1,5 @@
 # Nim-RocksDB
-# Copyright 2018-2019 Status Research & Development GmbH
+# Copyright 2018-2024 Status Research & Development GmbH
 # Licensed under either of
 #
 #  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
@@ -17,6 +17,8 @@ import
   ./options/test_dbopts,
   ./options/test_readopts,
   ./options/test_writeopts,
+  ./transactions/test_txdbopts,
+  ./transactions/test_txopts,
   ./test_backup,
   ./test_rocksdb,
   ./test_rocksiterator,
