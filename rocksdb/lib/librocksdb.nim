641d568b504119b1fb82618fe4439483ba998ce3
Jacek Sieka <jacek@status.im>
2024-06-03 22:53:10 +0200

    lint

--- rocksdb/lib/librocksdb.nim
+++ rocksdb/lib/librocksdb.nim
@@ -120,12 +120,12 @@ when defined(rocksdb_static_linking):
     topLevelPath = currentSourcePath.parentDir().parentDir().parentDir()
     libsDir = topLevelPath.replace('\\', '/') & "/build/lib"
 
-  {.passL: libsDir & "/librocksdb.a".}
-  {.passL: libsDir & "/liblz4.a".}
-  {.passL: libsDir & "/libzstd.a".}
+  {.passl: libsDir & "/librocksdb.a".}
+  {.passl: libsDir & "/liblz4.a".}
+  {.passl: libsDir & "/libzstd.a".}
 
   when defined(windows):
-    {.passL: "-lshlwapi -lrpcrt4".}
+    {.passl: "-lshlwapi -lrpcrt4".}
 
 else:
   when shouldUseNativeLinking():
