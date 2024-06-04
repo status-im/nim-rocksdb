277cbc5f5cd0171b8d4f870c4d16fb3efba744e0
Jacek Sieka <jacek@status.im>
2024-06-04 10:17:08 +0200

    obsolete todo

--- rocksdb/options/cache.nim
+++ rocksdb/options/cache.nim
@@ -8,7 +8,6 @@ type
     cPtr*: CachePtr
 
 proc cacheCreateLRU*(size: int): CacheRef =
-  # TODO do you release it?
   CacheRef(cPtr: rocksdb_cache_create_lru(size.csize_t))
 
 proc close*(cache: CacheRef) =
