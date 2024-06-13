e36f454cd4e3ed84f02952b5582327ef3a7a4f55
web3-developer <51288821+web3-developer@users.noreply.github.com>
2024-06-13 15:47:14 +0800

    Add helper script to cleanup build artifacts. (#47)

---
+++ scripts/clean_build_artifacts.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Nim-RocksDB
+# Copyright 2018-2024 Status Research & Development GmbH
+# Licensed under either of
+#
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+#  * GPL license, version 2.0, ([LICENSE-GPLv2](LICENSE-GPLv2) or https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+#
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
+
+rm -rf build
+make -C vendor/rocksdb clean
+
+git submodule foreach --recursive git clean -fdx
