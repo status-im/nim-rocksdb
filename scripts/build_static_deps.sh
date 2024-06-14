4eb3945d4337430a0e1cd25e23192b4a9134ee47
web3-developer <51288821+web3-developer@users.noreply.github.com>
2024-06-14 16:24:29 +0800

    If vendor RocksDb files have changed the trigger a clean rebuild.

--- scripts/build_static_deps.sh
+++ scripts/build_static_deps.sh
@@ -23,6 +23,14 @@ BUILD_DEST="${REPO_DIR}/build/lib"
 
 git submodule update --init
 
+if ${MAKE} -C "${ROCKSDB_LIB_DIR}" --dry-run static_lib | grep -q 'Nothing to be done'; then
+  echo "RocksDb static libraries already built. Skipping build."
+  exit 0
+else
+  ${REPO_DIR}/scripts/clean_build_artifacts.sh
+  echo "Building RocksDb static libraries."
+fi
+
 export DISABLE_WARNING_AS_ERROR=1
 
 export ROCKSDB_DISABLE_SNAPPY=1
