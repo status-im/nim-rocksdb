0c70516e22b027b15687a4cd773f82b5fbacf841
web3-developer <51288821+web3-developer@users.noreply.github.com>
2024-08-15 15:35:51 +0800

    Static link dependencies.

--- triplets/x64-linux-rocksdb.cmake
+++ triplets/x64-linux-rocksdb.cmake
@@ -1,7 +1,12 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_BUILD_TYPE release)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
-
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_FIXUP_ELF_RPATH ON)
+
+if(${PORT} MATCHES "snappy|zlib|lz4|zstd")
+  set(VCPKG_CRT_LINKAGE static)
+  set(VCPKG_LIBRARY_LINKAGE static)
+else()
+  set(VCPKG_CRT_LINKAGE dynamic)
+  set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
