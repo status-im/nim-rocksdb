9a57e93447365666c1490cf66882828f0e104297
bhartnett <51288821+bhartnett@users.noreply.github.com>
2024-12-16 22:08:38 +0800

    Use static VCPKG_CRT_LINKAGE on windows.

--- triplets/x64-windows-rocksdb.cmake
+++ triplets/x64-windows-rocksdb.cmake
@@ -2,7 +2,7 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_BUILD_TYPE release)
 
 if(${PORT} MATCHES "snappy|zlib|lz4|zstd")
-  set(VCPKG_CRT_LINKAGE dynamic)
+  set(VCPKG_CRT_LINKAGE static)
   set(VCPKG_LIBRARY_LINKAGE static)
 else()
   set(VCPKG_CRT_LINKAGE static)
