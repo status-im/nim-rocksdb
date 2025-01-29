124a29c059fd6a43798fa068cd78b9ac6a9390c9
bhartnett <51288821+bhartnett@users.noreply.github.com>
2025-01-29 09:52:20 +0800

    Write RocksDb version to file after successful build.

--- scripts/build_static_deps.sh
+++ scripts/build_static_deps.sh
@@ -21,15 +21,7 @@ BUILD_DEST="${REPO_DIR}/build"
 
 [[ -z "$NPROC" ]] && NPROC=2 # number of CPU cores available
 
-cd ${REPO_DIR}/vendor/rocksdb
-ROCKSDB_VERSION_BEFORE=$(${REPO_DIR}/vendor/rocksdb/build_tools/version.sh full)
-cd ${REPO_DIR}
-
 git submodule update --init
-
-cd ${REPO_DIR}/vendor/rocksdb
-ROCKSDB_VERSION_AFTER=$(${REPO_DIR}/vendor/rocksdb/build_tools/version.sh full)
-cd ${REPO_DIR}
 
 export DISABLE_WARNING_AS_ERROR=1
 export ROCKSDB_DISABLE_SNAPPY=1
@@ -41,9 +33,18 @@ export DEBUG_LEVEL=0
 if [ -f "${BUILD_DEST}/librocksdb.a" ] && \
    [ -f "${BUILD_DEST}/liblz4.a" ] && \
    [ -f "${BUILD_DEST}/libzstd.a" ] && \
-   [[ ${ROCKSDB_VERSION_BEFORE} == ${ROCKSDB_VERSION_AFTER} ]]; then
-  echo "RocksDb static libraries already built. Skipping build."
-  exit 0
+   [ -f "${BUILD_DEST}/version.txt" ]; then
+
+  ROCKSDB_VERSION_BEFORE=$(cat "${BUILD_DEST}/version.txt")
+
+  cd ${REPO_DIR}/vendor/rocksdb
+  ROCKSDB_VERSION_AFTER=$(${REPO_DIR}/vendor/rocksdb/build_tools/version.sh full)
+  cd ${REPO_DIR}
+
+  if [[ ${ROCKSDB_VERSION_BEFORE} == ${ROCKSDB_VERSION_AFTER} ]]; then
+    echo "RocksDb static libraries already built. Skipping build."
+    exit 0
+  fi
 fi
 
 if ${MAKE} -C "${ROCKSDB_LIB_DIR}" -q unity.a; then
@@ -54,6 +55,10 @@ if ${MAKE} -C "${ROCKSDB_LIB_DIR}" -q unity.a; then
   cp "${ROCKSDB_LIB_DIR}/liblz4.a" "${BUILD_DEST}/"
   cp "${ROCKSDB_LIB_DIR}/libzstd.a" "${BUILD_DEST}/"
   cp "${ROCKSDB_LIB_DIR}/unity.a" "${BUILD_DEST}/librocksdb.a"
+
+  cd ${REPO_DIR}/vendor/rocksdb
+  ${REPO_DIR}/vendor/rocksdb/build_tools/version.sh full > "${BUILD_DEST}/version.txt" 2>&1
+  cd ${REPO_DIR}
 
   exit 0
 else
@@ -75,3 +80,7 @@ mkdir -p "${BUILD_DEST}"
 cp "${ROCKSDB_LIB_DIR}/liblz4.a" "${BUILD_DEST}/"
 cp "${ROCKSDB_LIB_DIR}/libzstd.a" "${BUILD_DEST}/"
 cp "${ROCKSDB_LIB_DIR}/unity.a" "${BUILD_DEST}/librocksdb.a"
+
+cd ${REPO_DIR}/vendor/rocksdb
+${REPO_DIR}/vendor/rocksdb/build_tools/version.sh full > "${BUILD_DEST}/version.txt" 2>&1
+cd ${REPO_DIR}
