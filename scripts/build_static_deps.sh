5501a1cb7670f9eea54ae74c39fff5e35054e1c3
bhartnett <benjamin@status.im>
2026-09-02 06:00:14 +0200

    Read compression library versions from upstream rocksdb makefile.

--- scripts/build_static_deps.sh
+++ scripts/build_static_deps.sh
@@ -16,8 +16,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"/..
 REPO_DIR="${PWD}"
 ROCKSDB_LIB_DIR="${REPO_DIR}/vendor/rocksdb"
 BUILD_DEST="${REPO_DIR}/build"
-LZ4_VERSION=1.10.0
-ZSTD_VERSION=1.5.7
 LIBURING_VERSION=2.14
 LIBURING_DIR="${ROCKSDB_LIB_DIR}/liburing-liburing-${LIBURING_VERSION}"
 
@@ -26,6 +24,14 @@ LIBURING_DIR="${ROCKSDB_LIB_DIR}/liburing-liburing-${LIBURING_VERSION}"
 [[ -z "$NPROC" ]] && NPROC=2 # number of CPU cores available
 
 git submodule update --init
+
+LZ4_VERSION=$(sed -nE 's/^LZ4_VER \?= *([0-9.]+)[[:space:]]*$/\1/p' "${ROCKSDB_LIB_DIR}/Makefile")
+ZSTD_VERSION=$(sed -nE 's/^ZSTD_VER \?= *([0-9.]+)[[:space:]]*$/\1/p' "${ROCKSDB_LIB_DIR}/Makefile")
+
+if [[ -z "${LZ4_VERSION}" || -z "${ZSTD_VERSION}" ]]; then
+  echo "error: could not read LZ4_VER/ZSTD_VER from ${ROCKSDB_LIB_DIR}/Makefile" >&2
+  exit 1
+fi
 
 export DISABLE_WARNING_AS_ERROR=1
 export ROCKSDB_DISABLE_SNAPPY=1
